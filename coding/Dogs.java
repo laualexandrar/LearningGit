5440740a13e23796eea224c13f96dcb082d3bbe3
Laura Roa <laualexandrar@gmail.com>
2020-02-12 22:25:50 -0800

    making java programs

---
+++ coding/Dogs.java
@@ -0,0 +1,23 @@
+class Dogs{
+
+    String name;
+    int age = 5;
+    int size;
+
+
+   void bark(){
+   
+    if (age >= 11) {
+
+        System.out.println("WOFFF");
+    
+    } else if(age >=5) {
+
+        System.out.println("woooffff");
+
+    } else if(age >= 2){
+
+    System.out.println("gruauuu");
+   }
+}
+}
---
+++ coding/MainDogs.java
@@ -0,0 +1,7 @@
+class MainDogs{
+
+    public static void main (String[] args){
+            Dogs myDogs = new Dogs();
+            myDogs.bark();
+   }
+} 
